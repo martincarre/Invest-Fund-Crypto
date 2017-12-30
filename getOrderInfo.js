1ff13331bae97c5f74fb08ef2a0f498f7f89cd67
ardzii <m.carre86@gmail.com>
2017-12-31 00:37:13 +0100

    Added Balance check support

--- getOrderInfo.js
+++ getOrderInfo.js
@@ -1,14 +1,20 @@
 var _ = require("lodash");
 var ccxt = require("ccxt");
+var { keys } = require("./keys");
 
 let pairs = ["ETH/EUR", "BTC/EUR", "LTC/EUR", "BCH/EUR"];
-let exchanges = ["kraken", "bitfinex", "exmo", "bitstamp", "bitbay"];
+let exchanges = ["kraken", "bitfinex", "exmo", "bitbay"];
 
 function getOrder(p) {
   return Promise.all(
     ccxt.exchanges.map(async e => {
       if (exchanges.includes(e)) {
-        let exchange = new ccxt[e]({ timeout: 1500 });
+        let exchange = new ccxt[e]({
+          timeout: 1500,
+          apiKey: keys[e].api,
+          secret: keys[e].secret,
+          enableRateLimit: true
+        });
         if (exchange.hasFetchOrderBook) {
           let now = Math.floor(new Date());
           let order = await exchange
@@ -17,10 +23,30 @@ function getOrder(p) {
               return o;
             })
             .catch(err => {});
-          if (order) {
+          if (order && _.isObject(order)) {
+            let balance = await exchange
+              .fetchBalance()
+              .then(b => {
+                return b;
+              })
+              .catch(err => {});
             order.mkt = e;
             order.pair = p;
             order.ping = order.timestamp - now;
+            order.mktvar = {};
+            if (_.isObject(balance)) {
+              order.mktvar.balance = {
+                free: balance.free,
+                used: balance.used,
+                total: balance.total
+              };
+            } else {
+              order.mktvar.balance = {
+                free: "NA",
+                used: "NA",
+                total: "NA"
+              };
+            }
             return order;
           } else {
             return 3;
@@ -55,7 +81,11 @@ function transformOrder(base, comp) {
       basePriceSpread: base.asks[0][0] - comp.bids[0][0],
       compPriceSpread: comp.asks[0][0] - comp.bids[0][0],
       baseVolSpread: base.asks[0][1] - comp.bids[0][1],
-      compVilSpread: comp.asks[0][1] - base.bids[0][1]
+      compVolSpread: comp.asks[0][1] - base.bids[0][1]
+    },
+    oriBalanceInfo: {
+      baseBalance: base.mktvar.balance,
+      compBalance: comp.mktvar.balance
     },
     oriTimeInfo: {
       pingBase: base.ping,
